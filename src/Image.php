b489ba04fefa559b25f780a33355f893bef1ceb9
Òscar Casajuana <elboletaire@underave.net>
2015-06-28 21:21:36 +0200

    Added a lot of effect methods
    - Brightness
    - Colorize
    - Contrast
    - Edge Detection
    - Emboss
    - Grayscale
    - Sepia (combination of various effects)
    - Mean Removal
    - Negate
    - Smooth
    - Vignette (extracted from php.net comments)
    
    Added fitInRange and getColorArray methods

--- src/Image.php
+++ src/Image.php
@@ -181,11 +181,12 @@ class Image
             $bgcolor = -1;
         }
 
-        if ($bgcolor === -1) {
-            $bgcolor = imagecolorallocatealpha($this->image, 0, 0, 0, 127);
-        }
+        $color = $this->getColorArray($bgcolor);
+        $bgcolor = imagecolorallocatealpha($this->image, $color['r'], $color['g'], $color['b'], $color['a']);
 
         $this->image = imagerotate($this->image, $degrees * -1, $bgcolor);
+
+        $this->updateSize();
 
         return $this;
     }
@@ -285,9 +286,125 @@ class Image
                 throw new Exception("Incorrect blur type \"{$type}\"");
         }
 
-        for ($i = 0; $i < $passes; $i++) {
+        for ($i = 0; $i < $this->fitInRange($passes, 1); $i++) {
             imagefilter($this->image, $type);
         }
+
+        return $this;
+    }
+
+    /**
+     * Changes the brightness of the image.
+     *
+     * @param  integer $level Brightness value; range between -255 & 255.
+     * @return Image
+     */
+    public function brightness($level)
+    {
+        imagefilter(
+            $this->image,
+            IMG_FILTER_BRIGHTNESS,
+            $this->fitInRange($level, -255, 255)
+        );
+
+        return $this;
+    }
+
+    /**
+     * Like grayscale, except you can specify the color.
+     *
+     * @param  mixed  $color Color in any format accepted by getColorArray
+     * @return Image
+     */
+    public function colorize($color)
+    {
+        $color = $this->getColorArray($color);
+
+        imagefilter(
+            $this->image,
+            IMG_FILTER_COLORIZE,
+            $color['r'],
+            $color['g'],
+            $color['b'],
+            $color['a']
+        );
+
+        return $this;
+    }
+
+    /**
+     * Changes the contrast of the image.
+     *
+     * @param  integer $level Use for adjunting level of contrast (-100 to 100)
+     * @return Image
+     */
+    public function contrast($level)
+    {
+        imagefilter(
+            $this->image,
+            IMG_FILTER_CONTRAST,
+            $this->fitInRange($level, -100, 100)
+        );
+
+        return $this;
+    }
+
+    /**
+     * Uses edge detection to highlight the edges in the image.
+     *
+     * @return Image
+     */
+    public function edgeDetection()
+    {
+        imagefilter($this->image, IMG_FILTER_EDGEDETECT);
+
+        return $this;
+    }
+
+    /**
+     * Embosses the image.
+     *
+     * @return Image
+     */
+    public function emboss()
+    {
+        imagefilter($this->image, IMG_FILTER_EMBOSS);
+
+        return $this;
+    }
+
+    /**
+     * Applies grayscale filter.
+     *
+     * @return Image
+     */
+    public function grayscale()
+    {
+        imagefilter($this->image, IMG_FILTER_GRAYSCALE);
+
+        return $this;
+    }
+
+    /**
+     * Uses mean removal to achieve a "sketchy" effect.
+     *
+     * @return Image
+     */
+    public function meanRemove()
+    {
+        imagefilter($this->image, IMG_FILTER_MEAN_REMOVAL);
+
+        return $this;
+    }
+
+    /**
+     * Reverses all colors of the image.
+     *
+     * @return Image
+     */
+    public function negate()
+    {
+        imagefilter($this->image, IMG_FILTER_NEGATE);
 
         return $this;
     }
@@ -301,11 +418,77 @@ class Image
      */
     public function pixelate($block_size = 3, $advanced = false)
     {
-        if ($block_size <= 0) {
-            $block_size = 1;
-        }
+        imagefilter(
+            $this->image,
+            IMG_FILTER_PIXELATE,
+            $this->fitInRange($block_size, 1),
+            $advanced
+        );
 
-        imagefilter($this->image, IMG_FILTER_PIXELATE, $block_size, $advanced);
+        return $this;
+    }
+
+    /**
+     * A combination of various effects to achieve a sepia like effect.
+     *
+     * TODO: Create an additional class with instagram-like effects and move it there.
+     *
+     * @param  int   $alpha Defines the transparency of the effect: from 0 to 100
+     * @return Image
+     */
+    public function sepia($alpha = 0)
+    {
+        return $this
+            ->grayscale()
+            ->contrast(-3)
+            ->brightness(-15)
+            ->colorize([
+                'r' => 100,
+                'g' => 70,
+                'b' => 50,
+                'a' => $this->fitInRange($alpha, 0, 100)
+            ]
+        );
+    }
+
+    /**
+     * Makes the image smoother.
+     *
+     * @param  int   $level Level of smoothness, between -15 and 15.
+     * @return Image
+     */
+    public function smooth($level)
+    {
+        imagefilter(
+            $this->image,
+            IMG_FILTER_SMOOTH,
+            $this->fitInRange($level, -15, 15)
+        );
+
+        return $this;
+    }
+
+    /**
+     * Adds a vignette to image.
+     *
+     * @param  float  $size  Size of the vignette, between 0 and 10. Low is sharper.
+     * @param  float  $level Vignete transparency, between 0 and 1
+     * @return Image
+     * @link   http://php.net/manual/en/function.imagefilter.php#109809
+     */
+    public function vignette($size = 0.7, $level = 0.8)
+    {
+        for ($x = 0; $x < $this->width; ++$x) {
+            for ($y = 0; $y < $this->height; ++$y) {
+                $index = imagecolorat($this->image, $x, $y);
+                $rgb = imagecolorsforindex($this->image, $index);
+
+                $this->vignetteEffect($size, $level, $x, $y, $rgb);
+                $color = imagecolorallocate($this->image, $rgb['red'], $rgb['green'], $rgb['blue']);
+
+                imagesetpixel($this->image, $x, $y, $color);
+            }
+        }
 
         return $this;
     }
@@ -336,9 +519,130 @@ class Image
         return $this;
     }
 
+    /**
+     * Allows you to set the current image resource.
+     *
+     * This is intented for use it in conjuntion with getImage.
+     *
+     * @param [type] $image [description]
+     */
+    public function setImage($image)
+    {
+        if (!get_resource_type($image) == 'gd') {
+            throw new Exception("Given image is not a GD image resource");
+        }
+
+        $this->image = $image;
+        $this->updateSize();
+    }
+
+    /**
+     * Returns image resource, so you can use it however you wan.
+     *
+     * @return resource
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Returns metadata for current image.
+     *
+     * @return array
+     */
     public function getMetadata()
     {
         return $this->metadata;
+    }
+
+    /**
+     * Returns the proper color array for the given color.
+     *
+     * It accepts any (or almost any) imaginable type.
+     *
+     * @param  mixed  $color Can be an array (sequential or associative) or
+     *                       hexadecimal. In hexadecimal allows 3 and 6 characters
+     *                       for rgb and 4 or 8 characters for rgba.
+     * @return array         Containing all 4 color channels.
+     */
+    public static function getColorArray($color)
+    {
+        if ($color === -1) {
+            return [
+                'r' => 0,
+                'g' => 0,
+                'b' => 0,
+                'a' => 127
+            ];
+        }
+        if (is_array($color) && in_array(count($color), [3,4])) {
+            $allowed_keys = [
+                'associative' => ['red', 'green', 'blue', 'alpha'],
+                'reduced'     => ['r', 'g', 'b', 'a'],
+                'numeric'     => [0, 1, 2, 3]
+            ];
+
+            foreach ($allowed_keys as $keys) {
+                list($r, $g, $b, $a) = $keys;
+
+                if (!isset($color[$r], $color[$g], $color[$b])) {
+                    continue;
+                }
+
+                return [
+                    'r' => self::fitInRange($color[$r], 0, 255),
+                    'g' => self::fitInRange($color[$g], 0, 255),
+                    'b' => self::fitInRange($color[$b], 0, 255),
+                    'a' => self::fitInRange(isset($color[$a]) ? $color[$a] : 0, 0, 127),
+                ];
+            }
+
+            $encoded = json_encode($color);
+            throw new Exception("Invalid array color value $encoded");
+        }
+
+        if (is_string($color)) {
+            $color = ltrim($color, '#');
+            if (strlen($color) == 6) {
+                list($r, $g, $b) = [
+                    $color[0].$color[1],
+                    $color[2].$color[3],
+                    $color[4].$color[5]
+                ];
+            } elseif (strlen($color) == 3) {
+                list($r, $g, $b) = array(
+                    $color[0].$color[0],
+                    $color[1].$color[1],
+                    $color[2].$color[2]
+                );
+            } elseif (strlen($color) == 8) {
+                list($r, $g, $b, $a) = [
+                    $color[0].$color[1],
+                    $color[2].$color[3],
+                    $color[4].$color[5],
+                    $color[6].$color[7]
+                ];
+            } elseif (strlen($color) == 4) {
+                list($r, $g, $b, $a) = [
+                    $color[0].$color[0],
+                    $color[1].$color[1],
+                    $color[2].$color[2],
+                    $color[3].$color[3]
+                ];
+            } else {
+                throw new Exception("Invalid hexadecimal color value \"$color\"");
+            }
+
+            return [
+                'r' => hexdec($r),
+                'g' => hexdec($g),
+                'b' => hexdec($b),
+                'a' => isset($a) ? hexdec($a) : 0
+            ];
+        }
+
+        throw new Exception("Invalid color value \"$color\"");
     }
 
     /**
@@ -373,7 +677,7 @@ class Image
      */
     protected function getMetadataForImage()
     {
-        $this->metadata = self::getMetadataFromFile($this->filename);
+        $this->metadata = $this->getMetadataFromFile($this->filename);
 
         $this->width = $this->metadata['width'];
         $this->height = $this->metadata['height'];
@@ -403,6 +707,29 @@ class Image
     }
 
     /**
+     * Checks that the given value is between our defined range.
+     *
+     * Can check just for min or max if setting the other value to false.
+     *
+     * @param  int  $value Value to be checked,
+     * @param  bool $min   Minimum value. False to just use max.
+     * @param  bool $max   Maximum value. False to just use min.
+     * @return int         The value itself.
+     */
+    protected static function fitInRange($value, $min = false, $max = false)
+    {
+        if ($min !== false && $value < $min) {
+            $value = $min;
+        }
+
+        if ($max !== false && $value > $max) {
+            $value = $max;
+        }
+
+        return $value;
+    }
+
+    /**
      *  Applies some values to image for handling transparency
      *
      * @return void
@@ -411,5 +738,38 @@ class Image
     {
         imagesavealpha($image, true);
         imagealphablending($image, true);
+    }
+
+    /**
+     * Resets width and height of the current image.
+     *
+     * @return void
+     */
+    protected function updateSize()
+    {
+        $this->width  = imagesx($this->image);
+        $this->height = imagesy($this->image);
+    }
+
+    /**
+     * Required by vignette to generate the propper colors.
+     *
+     * @param  float  $size  Size of the vignette, between 0 and 10. Low is sharper.
+     * @param  float  $level Vignete transparency, between 0 and 1
+     * @param  int    $x     X position of the pixel.
+     * @param  int    $y     Y position of the pixel.
+     * @param  array  &$rgb  Current pixel olor information.
+     * @return void
+     */
+    protected function vignetteEffect($size , $level, $x, $y, &$rgb)
+    {
+        $l = sin(M_PI / $this->width * $x) * sin(M_PI / $this->height * $y);
+        $l = pow($l, $this->fitInRange($size , 0, 10));
+
+        $l = 1 - $this->fitInRange($level, 0, 1) * (1 - $l);
+
+        $rgb['red'] *= $l;
+        $rgb['green'] *= $l;
+        $rgb['blue'] *= $l;
     }
 }
