0bdef20ba2ae0f7203b9c56e1bca78ef3d3c1ba6
Òscar Casajuana <elboletaire@underave.net>
2015-09-11 19:39:16 +0200

    Fixed Watimage example to work with new location

--- examples/Watimage.php
+++ examples/Watimage.php
@@ -1,20 +1,26 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-
 date_default_timezone_set('UTC');
 
-require 'src/watimage.php';
+$current_path = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+$files_path = $current_path . 'files' . DIRECTORY_SEPARATOR;
+$output_path = $current_path . 'output' . DIRECTORY_SEPARATOR;
+$image = $files_path . 'test.png';
+$watermark = $files_path . 'watermark.png';
 
+require realpath($current_path . '../vendor/autoload.php');
+
+use Elboletaire\Watimage\Watimage;
 
 /************************
  *** APPLY WATERMARKS ***
  ************************/
-$wm = new Elboletaire\Watimage\Watimage();
-$wm->setImage(array('file' => 'test.png', 'quality' => 70)); // file to use and export quality
-$wm->setWatermark(array('file' => 'watermark.png', 'position' => 'top right')); // watermark to use and its position
+$wm = new Watimage();
+$wm->setImage(array('file' => $image, 'quality' => 70)); // file to use and export quality
+$wm->setWatermark(array('file' => $watermark, 'position' => 'top right')); // watermark to use and its position
 $wm->applyWatermark(); // apply watermark to the canvas
-if ( !$wm->generate('test1.png') ) {
+if ( !$wm->generate($output_path . 'test1.png') ) {
 	// handle errors...
 	print_r($wm->errors);
 }
@@ -22,10 +28,10 @@ if ( !$wm->generate('test1.png') ) {
 /*********************
  *** RESIZE IMAGES ***
  *********************/
-$wm = new Elboletaire\Watimage\Watimage('test.png');
+$wm = new Watimage($image);
 // allowed types: resize, resizecrop, resizemin, crop and reduce
 $wm->resize(array('type' => 'resizecrop', 'size' => array(400, 200)));
-if ( !$wm->generate('test2.png') ) {
+if ( !$wm->generate($output_path . 'test2.png') ) {
 	// handle errors...
 	print_r($wm->errors);
 }
@@ -34,9 +40,9 @@ if ( !$wm->generate('test2.png') ) {
 /*********************
  *** ROTATE IMAGES ***
  *********************/
-$wm = new Elboletaire\Watimage\Watimage('test.png');
+$wm = new Watimage($image);
 $wm->rotate(90);
-if ( !$wm->generate('test3.png') ) {
+if ( !$wm->generate($output_path . 'test3.png') ) {
 	// handle errors...
 	print_r($wm->errors);
 }
@@ -44,8 +50,8 @@ if ( !$wm->generate('test3.png') ) {
 /**********************************
  *** EXPORTING TO OTHER FORMATS ***
  **********************************/
-$wm = new Elboletaire\Watimage\Watimage('test.png');
-if ( !$wm->generate('test4.jpg', 'image/jpeg') ) {
+$wm = new Watimage($image);
+if ( !$wm->generate($output_path . 'test4.jpg', 'image/jpeg') ) {
 	// handle errors...
 	print_r($wm->errors);
 }
@@ -53,9 +59,9 @@ if ( !$wm->generate('test4.jpg', 'image/jpeg') ) {
 /*******************
  *** FLIP IMAGES ***
  *******************/
-$wm = new Elboletaire\Watimage\Watimage('test.png');
+$wm = new Watimage($image);
 $wm->flip('vertical'); // or "horizontal"
-if ( !$wm->generate('test5.png') ) {
+if ( !$wm->generate($output_path . 'test5.png') ) {
 	// handle errors...
 	print_r($wm->errors);
 }
@@ -65,14 +71,14 @@ if ( !$wm->generate('test5.png') ) {
  *** CROPPING IMAGES ***
  ***********************/
 // Usefull for cropping plugins like https://github.com/tapmodo/Jcrop
-$wm = new Elboletaire\Watimage\Watimage('test.png');
+$wm = new Watimage($image);
 $wm->crop(array( // values from the cropper
 	'width' => 500, // the cropped width
 	'height' => 500, // "     "	   height
 	'x' => 50,
 	'y' => 80
 ));
-if ( !$wm->generate('test6.png') ) {
+if ( !$wm->generate($output_path . 'test6.png') ) {
 	// handle errors...
 	print_r($wm->errors);
 }
@@ -82,17 +88,17 @@ if ( !$wm->generate('test6.png') ) {
  *** EVERYTHING TOGETHER ***
  ***************************/
 
-$wm = new Elboletaire\Watimage\Watimage();
+$wm = new Watimage();
 
 // Set the image
-$wm->setImage('test.png'); // you can also set the quality with setImage, you only need to change it with an array: array('file' => 'test.png', 'quality' => 70)
+$wm->setImage($image); // you can also set the quality with setImage, you only need to change it with an array: array('file' => $image, 'quality' => 70)
 
 // Set the export quality
 $wm->setQuality(80);
 
 // Set a watermark
 $wm->setWatermark(array(
-	'file' => 'watermark.png',  // the watermark file
+	'file' => $watermark,  // the watermark file
 	'position' => 'center center', // the watermark position works like CSS backgrounds positioning
 	'margin' => array('x' => -20, 'y' => 10), // you can set some 'margins' to the watermark for better positioning
 	'size' => 'full' // you can set the size of the watermark using a percentage or the word "full" for getting a full width/height watermark
@@ -111,9 +117,11 @@ $wm->rotate(30);
 $wm->applyWatermark();
 
 // Export the file
-if ( !$wm->generate('test7.png') ) {
+if ( !$wm->generate($output_path . 'test7.png') ) {
 	// handle errors...
 	print_r($wm->errors);
 }
 
 // END OF FILE
+
+echo "All examples are now available under the 'output' folder\n";
