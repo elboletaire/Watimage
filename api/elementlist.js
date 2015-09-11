aadcb9e29b8f9490769259ef8fb32a4021e40cac
Òscar Casajuana <elboletaire@underave.net>
2015-09-12 00:35:41 +0200

    First commit with just the API

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Elboletaire\\Watimage\\Exception\\ExtensionNotLoadedException"],["c","Elboletaire\\Watimage\\Exception\\FileNotExistException"],["c","Elboletaire\\Watimage\\Exception\\InvalidArgumentException"],["c","Elboletaire\\Watimage\\Exception\\InvalidExtensionException"],["c","Elboletaire\\Watimage\\Exception\\InvalidMimeException"],["c","Elboletaire\\Watimage\\Image"],["c","Elboletaire\\Watimage\\Normalize"],["c","Elboletaire\\Watimage\\Watermark"],["c","Elboletaire\\Watimage\\Watimage"]];
