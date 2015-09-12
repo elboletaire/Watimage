9b95902819297ee26f9b82cf2b859c8ab89a80ac
Òscar Casajuana <elboletaire@underave.net>
2015-09-12 15:21:12 +0200

    Preparing for travis

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+require 'vendor/autoload.php';
+
+if (!defined('IMG_FLIP_HORIZONTAL')) {
+    define('IMG_FLIP_HORIZONTAL', 1);
+}
+
+if (!defined('IMG_FLIP_VERTICAL')) {
+    define('IMG_FLIP_VERTICAL', 2);
+}
+
+if (!defined('IMG_FLIP_BOTH')) {
+    define('IMG_FLIP_BOTH', 3);
+}
+
+// And that's it :_D
