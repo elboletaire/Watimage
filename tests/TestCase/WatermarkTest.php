668767cea6a672d42eb7484ba974f76cae8e2036
Òscar Casajuana <elboletaire@underave.net>
2015-09-13 21:27:40 +0200

    Filled test for apply (which was empty by accident)

--- tests/TestCase/WatermarkTest.php
+++ tests/TestCase/WatermarkTest.php
@@ -31,9 +31,80 @@ class WatermarkTest extends TestCaseBase
         $this->assertArraySubset([0, 0], $this->getProperty('margin'));
     }
 
+    /**
+     * @covers Elboletaire\Watimage\Watermark::calculatePosition
+     * @uses   Elboletaire\Watimage\Image
+     */
     public function testApply()
     {
+        // Check values
+        $red = [
+            'red' => 255,
+            'green' => 0,
+            'blue' => 0,
+            'alpha' => 0
+        ];
+        $white = [
+            'red' => 255,
+            'green' => 255,
+            'blue' => 255,
+            'alpha' => 0
+        ];
+        $transparent = [
+            'red' => 0,
+            'green' => 0,
+            'blue' => 0,
+            'alpha' => 127
+        ];
 
+        // Init classes
+        $image = new Image();
+        $watermark = $this->testClass;
+
+        $image->create(200)->fill($white);
+        $instance = $watermark->create(10)
+            ->fill($red)
+            ->setPosition('top left')
+            ->apply($image)
+        ;
+
+        // Check instances are the expected ones
+        $this->assertInstanceOf('Elboletaire\Watimage\Image', $image);
+        $this->assertInstanceOf('Elboletaire\Watimage\Image', $instance);
+        $this->assertInstanceOf('Elboletaire\Watimage\Watermark', $watermark);
+
+        $resource = $image->getImage();
+
+        // Ensure whatermark is there
+        $pixel = imagecolorsforindex($resource, imagecolorat($resource, 0, 0));
+        $this->assertArraySubset($red, $pixel);
+        $pixel = imagecolorsforindex($resource, imagecolorat($resource, 9, 9));
+        $this->assertArraySubset($red, $pixel);
+        $pixel = imagecolorsforindex($resource, imagecolorat($resource, 9, 0));
+        $this->assertArraySubset($red, $pixel);
+        $pixel = imagecolorsforindex($resource, imagecolorat($resource, 0, 9));
+        $this->assertArraySubset($red, $pixel);
+
+        // Check image is just after the image
+        $pixel = imagecolorsforindex($resource, imagecolorat($resource, 10, 10));
+        $this->assertArraySubset($white, $pixel);
+        $pixel = imagecolorsforindex($resource, imagecolorat($resource, 0, 10));
+        $this->assertArraySubset($white, $pixel);
+        $pixel = imagecolorsforindex($resource, imagecolorat($resource, 10, 0));
+        $this->assertArraySubset($white, $pixel);
+
+        // Check transparencies
+        $image->create(400);
+        $watermark
+            ->load("{$this->files_path}/watermark.png")
+            ->setPosition('top left')
+            ->apply($image)
+        ;
+
+        $resource = $image->getImage();
+        // Ensure it's still transparent
+        $pixel = imagecolorsforindex($resource, imagecolorat($resource, 0, 0));
+        $this->assertArraySubset($transparent, $pixel);
     }
 
     /**
